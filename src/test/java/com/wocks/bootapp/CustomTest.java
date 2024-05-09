5238aa7bab9ae664d5beddd3b8e6d67b9a32d26f
wocks1123 <122430128+DevRyu10@users.noreply.github.com>
2024-05-09 23:33:08 +0900

    test

---
+++ src/test/java/com/wocks/bootapp/CustomTest.java
@@ -0,0 +1,22 @@
+package com.wocks.bootapp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CustomTest {
+
+    @Test
+    @DisplayName("1 + 2 = 3")
+    public void test01() {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        assertThat(a + b).isEqualTo(3);
+    }
+
+}
